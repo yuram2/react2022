3c20080a864f33b1a596add4471f4249b286b3ea
yuram2 <lur0872@gmail.com>
2022-04-20 12:12:04 +0900

    1

--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -5,19 +5,92 @@ import Footer from "../layout/Footer";
 import Title from "../layout/Title";
 import Contact from "../layout/Contact";
 import ReferCont from "../includes/ReferCont";
-
-function Reference(){
-    return (
-        <>
-            <Header color="light" />
-            <Contents>
-                <Title title={["reference","book"]} color="light" />
-                <ReferCont color="light" />
-                <Contact />
-            </Contents>
-            <Footer color="light" />
-        </>
-    )
+import Loading from "../basics/Loading";
+import { gsap } from "gsap";
+// function Reference(){
+//     return (
+//         <>
+//             <Header color="light" />
+//             <Contents>
+//                 <Title title={["reference","book"]} color="light" />
+//                 <ReferCont color="light" />
+//                 <Contact />
+//             </Contents>
+//             <Footer color="light" />
+//         </>
+//     )
+// }
+class Reference extends React.Component {
+    state = {
+        isLoading: true,
+    }
+    mainAnimation = () => {
+        setTimeout(() => {
+            gsap.to("#header", {
+                duration: 0.8,
+                top: 0,
+            });
+            gsap.to("#footer", {
+                duration: 0.8,
+                bottom: 0,
+                delay: 0.2,
+            });
+            gsap.to(".cont__title strong", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.0,
+                ease: "power4.out"
+            });
+            gsap.to(".cont__title em", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.3,
+                ease: "power4.out"
+            });
+            gsap.to(".refer__inner", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.5,
+                ease: "power4.out"
+            });
+        }, 10)
+    }
+    getSite = () => {
+        setTimeout(() => {
+            console.log("두번째 시작")
+            this.setState({isLoading: false});
+            this.mainAnimation();
+        }, 1600)
+    }
+    componentDidMount(){
+        setTimeout(() => {
+            document.getElementById("loading").classList.remove("loading__active");
+            document.querySelector("body").style.background = "#F0EEEB";
+            this.getSite();
+        }, 2000);
+    }
+    render(){
+        const {isLoading} = this.state;
+        return (
+            <>
+                {isLoading ? (
+                    <Loading color="light" />
+                ) : (
+                    <>
+                        <Header color="light" />
+                        <Contents>
+                            <Title title={["Reference","book"]} color="light" />
+                            <ReferCont color="light" />
+                            <Contact />
+                        </Contents>
+                        <Footer color="light" />
+                    </>
+                )}
+            </>
+        )
+    }
 }
-
 export default Reference;
