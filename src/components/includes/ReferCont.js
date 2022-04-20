4b1336ac14ee01a6dcaeb3ba0fe17ac2f522959a
yuram2 <lur0872@gmail.com>
2022-04-20 14:57:47 +0900

    0420A

--- src/components/includes/ReferCont.js
+++ src/components/includes/ReferCont.js
@@ -12,8 +12,8 @@ function referCont(props) {
               <col style={{width: "20%"}} />
               <col style={{width: "70%"}} />
             </colgroup>
-            <tbody>
-              <tr>
+            <tbody> 
+              <tr> 
                 <td>1</td>
                 <td>align-content</td>
                 <td>all 속성은 요소의 속성을 초기화 또는 상속을 설정합니다.</td>
